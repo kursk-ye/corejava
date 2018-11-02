f681515ab525209c25936e5d49619c7d71820046
yelei <kursk.ye@gmail.com>
2018-11-02 10:13:21 +0800

    generic

--- src/generic/fruits/Fruit.java
+++ src/generic/fruits/Fruit.java
@@ -2,7 +2,7 @@ package generic.fruits;
 
 import java.util.function.DoubleConsumer;
 
-public class Fruit {
+public class Fruit implements Comparable<Fruit> {
     private  double price;
 
     public Fruit(){
@@ -18,7 +18,11 @@ public class Fruit {
         return "I am a Fruit !!";
     }
 
-
+    public int compareTo(Fruit o) {
+        if (price == o.price) return 0;
+        if (price > o.price ) return 1;
+            return -1;
+    }
 }
 
 
