862d31fc9abadd4b5f3c13dce4e097188448b51d
kursk.ye@gmail.com <kursk.ye@gmail.com>
2018-11-11 20:17:58 +0800

    method reference example without generic

---
+++ src/lambda/reference/MethodProvider.java
@@ -0,0 +1,17 @@
+package lambda.reference;
+
+import static com.kursk.Util.println;
+
+public class MethodProvider {
+    private String provider;
+
+    MethodProvider(String provider)
+    {
+        this.provider = provider;
+    }
+
+    public void helloProvider() {
+        println("Hello " + this.provider + " !")  ;
+        // return "Hello ";
+    }
+}
