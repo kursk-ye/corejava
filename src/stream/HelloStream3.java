da50fdaf64ba37c996a946ad7d5deea4f44ed58f
kursk.ye@gmail.com <kursk.ye@gmail.com>
2019-11-27 00:25:01 +0800

    vote

---
+++ src/stream/HelloStream3.java
@@ -0,0 +1,17 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.kursk.Util.println;
+
+public class HelloStream3 {
+    public static void main(String[] args) {
+        List<String> wordList = Arrays.asList("test1","test2","test3","test4");
+        wordList.stream().filter( w -> w.length() > 3);
+        wordList.stream().map(String::toUpperCase).forEach( w -> println(w));
+        wordList.stream().map( s -> s.substring(0,4)).forEach( s -> println(s));
+
+    }
+}
