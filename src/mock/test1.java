407691f4b46df884e8fba112e0a4ec411b3ca298
kursk-ye <2014c++expert>
2020-11-11 00:11:53 +0800

    add mockito

---
+++ src/mock/test1.java
@@ -0,0 +1,12 @@
+package mock;
+
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+public class Test1 {
+  public static void main(String[] args) {
+    //
+      List mockedList = mock(List.class);
+  }
+}
